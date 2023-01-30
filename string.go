07e12c5a6dbed2002af1b4cf72c9553552574b2a
Kafaa Billahi Syahida <kafaabillaahisyahida135@gmail.com>
2023-01-30 18:06:22 +0700

    String Function

--- string.go
+++ string.go
@@ -6,4 +6,11 @@ func main() {
 	fmt.Println("Kafaa")
 	fmt.Println("Kafaa Billahi")
 	fmt.Println("Kafaa Billahi Syahida")
+
+	fmt.Println("len(Kafaa) =", len("Kafaa"))
+	fmt.Println("Kafaa[0] =", "Kafaa"[0])
+	fmt.Println("Kafaa[1] =", "Kafaa"[1])
+
+	fmt.Println("Kafaa[0] =", string("Kafaa"[0]))
+	fmt.Println("Kafaa[1] =", string("Kafaa"[1]))
 }
