7fa50019989fbdfc9d8dbe2464d2d2a410f67024
Kafaa Billahi Syahida <kafaabillaahisyahida135@gmail.com>
2023-01-30 18:10:24 +0700

    Variabel

---
+++ variable.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Budi"
+	fmt.Println(name)
+
+	name = "Badu"
+	fmt.Println(name)
+
+	// error :
+	// name = 100
+	// name = 1.23
+}
